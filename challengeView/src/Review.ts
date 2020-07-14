3d447201b6de97fdf70498079fd9a66221414327
Lee <leejsitu@gmail.com>
2020-07-14 16:46:45 +0900

    Review interface

--- challengeView/src/Review.ts
+++ challengeView/src/Review.ts
@@ -1,9 +1,9 @@
 export interface Review{
     id: number;
     reviewer: number;
-    reviewer_name: string;
+    reviewer_name?: string;
     reviewee: number;
-    reviewee_name: string;
+    reviewee_name?: string;
     comments: string;
     created: Date;
     updated: Date;
